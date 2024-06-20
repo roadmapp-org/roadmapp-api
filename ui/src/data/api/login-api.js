1aad3b74d954b1b272c1423ee46329c4430e4bc1
Tobias Olea <tobiolea97@gmail.com>
2024-06-20 12:45:39 -0300

    Restructuring project: I have now an single repo for the api and a single repo for the ui

--- ui/src/data/api/login-api.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export async function login(credentials) {
-    const response = await axios.post('http://localhost:8080/auth/login', credentials);
-    return response.data;
-}
